c9c03c50172c1442c528fc6995a600e935c54fdb
songhee <songhee172@gmail.com>
2022-06-01 02:13:31 +0900

    2. Authentication

---
+++ src/components/Router.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { HashRouter as Router, Route, Routes, Redirect } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+import Profile from "../routes/Profile"
+import Navigation from "./Navigation";
+
+const AppRouter = ({isLoggedIn}) => {
+  return (
+    <Router>
+      {isLoggedIn && <Navigation />}
+      <Routes>
+        {isLoggedIn ? (
+          <> 
+          <Route exact path="/" element={<Home />} />
+          <Route exct path="/profile" element={<Profile />} />
+          </>
+        ):(
+          <>
+            <Route exact path="/" element={<Auth />} />
+          </>
+        )}
+      </Routes>
+    </Router>
+  );
+};
+export default AppRouter;
---
+++ src/routes/Profile.js
@@ -0,0 +1,16 @@
+import { authService } from "fbase";
+import React from "react";
+import { useNavigate } from "react-router-dom"
+
+export default () => {
+    const navigate = useNavigate();
+    const onLogOutClick = () => {
+        authService.signOut();
+        navigate("/");
+    };
+    return (
+        <>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    )
+};
